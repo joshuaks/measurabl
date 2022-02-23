84696e481a5ff275169540cefca72063be3ab9c0
joshuaks <5505155+joshuaks@users.noreply.github.com>
2022-02-23 10:08:03 -0500

    fix names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output api_endpoint {
   description = "api endpoint which gets counter value and updates"
-  value = aws_apigatewayv2_api._.api_endpoint
+  value = aws_apigatewayv2_api.endpoint.api_endpoint
 }
 
 output website_endpoint {
   description = "the website which displays the counter value"
-  value = aws_s3_bucket_website_configuration._.website_endpoint
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
